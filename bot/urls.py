5eca515b484c9f9df11e5142397cf7ad2c9bc211
ilyakrasnovsky <ilyakrasnovsky@gmail.com>
2016-11-12 13:23:54 -0500

    urls.py again?

--- bot/urls.py
+++ bot/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
  	url(r'^$', views.index, name='index'),
-	url(r'^behave/(?P<victimID>\d+)/$', views.behave, name='behave'),
- 	url(r'^boobot_ilya/$', views.boobot_ilya, name='boobot_ilya'),
- 	url(r'^boobot_dorothy/$', views.boobot_dorothy, name='boobot_dorothy'),	
- 	url(r'^start_the_fun/$', views.start_the_fun, name='start_the_fun'),
- 	url(r'^end_the_fun/$', views.end_the_fun, name='end_the_fun'),
+	url(r'^behave/(?P<victimID>\d+)/', views.behave, name='behave'),
+ 	url(r'^boobot_ilya$', views.boobot_ilya, name='boobot_ilya'),
+ 	url(r'^boobot_dorothy$', views.boobot_dorothy, name='boobot_dorothy'),	
+ 	url(r'^start_the_fun$', views.start_the_fun, name='start_the_fun'),
+ 	url(r'^end_the_fun$', views.end_the_fun, name='end_the_fun'),
 ]
