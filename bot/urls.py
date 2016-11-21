444cab8c8222d5751636285dc108fb94bfc9cbce
ilyakrasnovsky <ilyakrasnovsky@gmail.com>
2016-11-20 21:11:50 -0500

    forgot url links

--- bot/urls.py
+++ bot/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
 	url(r'^behave/(?P<victimID>\d+)$', views.behave, name='behave'),
  	url(r'^start_the_fun/$', views.start_the_fun, name='start_the_fun'),
  	url(r'^end_the_fun/$', views.end_the_fun, name='end_the_fun'),
+ 	url(r'^activate_the_fun/$', views.activate_the_fun, name='activate_the_fun'),
+ 	url(r'^deactivate_the_fun/$', views.deactivate_the_fun, name='deactivate_the_fun'),	
 ]
